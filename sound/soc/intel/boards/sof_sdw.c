974601a7744488b351612773c2092303fc946f64
Chao Song <chao.song@linux.intel.com>
2023-05-18 11:05:24 +0800

    ASoC: Intel: sof_sdw: add cs35l56 and cs42l43 codec info
    Need to select cs42l43 config, too. Will add it when cs42l43 codec
    driver is ready.
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Signed-off-by: Chao Song <chao.song@linux.intel.com>

--- sound/soc/intel/boards/sof_sdw.c
+++ sound/soc/intel/boards/sof_sdw.c
@@ -745,6 +745,29 @@ static struct sof_sdw_codec_info codec_info_list[] = {
 		.dai_num = 1,
 	},
 	{
+		.part_id = 0x3556,
+		.dais = {
+			{
+				.direction = {true, true},
+				.dai_name = "cs35l56-sdw1",
+				.dai_type = SOF_SDW_DAI_TYPE_AMP,
+				.dailink = {SDW_AMP_OUT_DAI_ID, SDW_AMP_IN_DAI_ID},
+			}
+		},
+		.dai_num = 1,
+	},
+	{
+		.part_id = 0x4243,
+		.dais = {
+			{
+				.direction = {true, true},
+				.dai_name = "cs4243-aif",
+				.dai_type = SOF_SDW_DAI_TYPE_JACK,
+				.dailink = {SDW_JACK_OUT_DAI_ID, SDW_JACK_IN_DAI_ID},
+			}
+		}
+	},
+	{
 		.part_id = 0xaaaa, /* generic codec mockup */
 		.version_id = 0,
 		.dais = {
